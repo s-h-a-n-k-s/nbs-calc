13d79de0eed7e0fd11594b49c704954f938f2205
s-h-a-n-k-s <ramsdentom@gmail.com>
2019-06-24 15:46:47 +0100

    Complete Divide feature

--- src/test/java/com/qa/nbs/calculator/CalculatorTest.java
+++ src/test/java/com/qa/nbs/calculator/CalculatorTest.java
@@ -19,4 +19,12 @@ public class CalculatorTest {
 
 		Assert.assertEquals(4, result);
 	}
+
+	@Test
+	public void divideTwoNumbersTest() {
+		Calculator calculator = new Calculator();
+		int result = calculator.divide(4, 2);
+
+		Assert.assertEquals(2, result);
+	}
 }
